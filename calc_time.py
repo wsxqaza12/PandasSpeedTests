08198997a05665168cb911d99b0458c17715b240
chichi <wsxqaza12@gmail.com>
2023-12-31 03:44:19 +0800

    groupby_agg2

--- calc_time.py
+++ calc_time.py
@@ -8,3 +8,12 @@ def time_read_csv(read_function, file_path):
     del df  
     gc.collect()
     return read_time
+
+def time_groupby(read_function, groupby_function, file_path):
+    df = read_function(file_path)
+    start_time = time.time()
+    result = groupby_function(df)
+    total_time = time.time() - start_time
+    del df, result
+    gc.collect()
+    return total_time
